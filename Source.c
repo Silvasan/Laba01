adacc9fabb0b4fdf958d28beb23680f21a6f12b8
Silvasan <yfghjkhg@gmail.com>
2020-11-10 21:11:09 +0300

    +check numbers

--- Source.c
+++ Source.c
@@ -3,10 +3,41 @@
 
 int main() {
 	int N = 0, M = 0, i = 1;
-	printf("Enter N: ");
-	scanf("%d", &N);
-	printf("Enter M: ");
-	scanf("%d", &M);
+	char bufStub = 0;			//buffer
+	while (1) {					//infinity while
+		bufStub = 0;
+		printf("Enter integer number N: ");
+		if (!scanf("%d", &N)) {
+			printf("Wrong input\n");
+			while (bufStub != '\n')			//buffercleaner
+				scanf("%c", &bufStub);
+		}
+		else {
+			scanf("%c", &bufStub);
+			if (bufStub == '\n')
+				break;
+			printf("Wrong input\n");
+			while (bufStub != '\n')
+				scanf("%c", &bufStub);
+		}
+	}
+	while (1) {
+		bufStub = 0;
+		printf("Enter integer number M: ");
+		if (!scanf("%d", &M)) {
+			printf("Wrong input\n");
+			while (bufStub != '\n')
+				scanf("%c", &bufStub);
+		}
+		else {
+			scanf("%c", &bufStub);
+			if (bufStub == '\n')
+				break;
+			printf("Wrong input\n");
+			while (bufStub != '\n')
+				scanf("%c", &bufStub);
+		}
+	}
 	while (i * M <= N) {
 		if (N == i * M) {
 			printf("TRUE\n");
