331fb53353881d5029d59eb55eff35ef5a1cd9c5
Silvasan <yfghjkhg@gmail.com>
2020-11-27 07:16:57 +0300

    +returns

--- Source.c
+++ Source.c
@@ -1,43 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
 	int N = 0, M = 0, i = 1;
-	char bufStub = 0;			//buffer
-	while (1) {					//infinity while
+	char bufStub = 0;
+	while (1) {
 		bufStub = 0;
 		printf("Enter integer number N: ");
-		if (!scanf("%d", &N)) {
-			printf("Wrong input\n");
-			while (bufStub != '\n')			//buffercleaner
-				scanf("%c", &bufStub);
-		}
+		if (!scanf("%d", &N))
+			return -1;
 		else {
 			scanf("%c", &bufStub);
 			if (bufStub == '\n')
 				break;
-			printf("Wrong input\n");
-			while (bufStub != '\n')
-				scanf("%c", &bufStub);
+			return -1;
 		}
 	}
 	while (1) {
 		bufStub = 0;
 		printf("Enter integer number M: ");
-		if (!scanf("%d", &M)) {
-			printf("Wrong input\n");
-			while (bufStub != '\n')
-				scanf("%c", &bufStub);
-		}
+		if (!scanf("%d", &M))
+			return -1;
 		else {
 			scanf("%c", &bufStub);
 			if (bufStub == '\n')
 				break;
-			printf("Wrong input\n");
-			while (bufStub != '\n')
-				scanf("%c", &bufStub);
+			return -1;
 		}
 	}
+	if (N < 0)
+		N *= -1;
+	if (M < 0)
+		M *= -1;
 	while (i * M <= N) {
 		if (N == i * M) {
 			printf("TRUE\n");
@@ -47,6 +40,6 @@ int main() {
 	}
 	if (!(N == i * M))
 		printf("FALSE\n");
-	system("pause");
+	getchar();
 	return 0;
 }
