ce0574d734797f356a5aae1e48f09251e02ef578
Silvasan <74051655+Silvasan@users.noreply.github.com>
2020-12-04 09:31:17 +0300

    Perfect

--- Source.c
+++ Source.c
@@ -3,53 +3,56 @@
 int main() {
 	int N = 0, M = 0;
 	char bufStub = 0;
-	while (1) {
+	while (1) {					//infinity while
 		bufStub = 0;
 		printf("Enter integer number N: ");
-		if (!scanf("%d", &N))
-			return -1;
+		if (!scanf("%d", &N)) {
+			printf("Wrong input\n");
+			while (bufStub != '\n')			//buffercleaner
+				scanf("%c", &bufStub);
+		}
 		else {
 			scanf("%c", &bufStub);
 			if (bufStub == '\n')
 				break;
-			return -1;
+			printf("Wrong input\n");
+			while (bufStub != '\n')
+				scanf("%c", &bufStub);
 		}
 	}
 	while (1) {
 		bufStub = 0;
 		printf("Enter integer number M: ");
-		if (!scanf("%d", &M))
-			return -1;
+		if (!scanf("%d", &M)) {
+			printf("Wrong input\n");
+			while (bufStub != '\n')
+				scanf("%c", &bufStub);
+		}
 		else {
 			scanf("%c", &bufStub);
 			if (bufStub == '\n')
 				break;
-			return -1;
+			printf("Wrong input\n");
+			while (bufStub != '\n')
+				scanf("%c", &bufStub);
 		}
 	}
+	
+
 	do {
 		if (N == 0 || M == 0) {
 			printf("Error!");
 			break;
-		}
-
-		if (N > M) {
-			if (N % M == 0) {
-				printf("GCD = %d", M);
-				break;
 			}
-			else N %= M;
-		}
-
-		else {
-			if (M % N == 0) {
-				printf("GCD = %d", N);
-				break;
-			}
-			else
-				M %= N;
-		}
-	} while (N != 0 || M != 0);
+		if (N < 0)
+			N *= (-1);
+		if (M < 0)
+			M *= (-1);
+		if (N > M) 
+			N -= M;
+		else M -= N;
+	} while (N != M);
+	printf("GCD = %d", N);
 	getchar();
 	return 0;
 }
